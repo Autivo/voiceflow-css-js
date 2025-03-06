f5064cee007adb6b1b3109257c3c0e74e3e54784
sperezmasri <sperezmasri@uc.cl>
2025-03-06 12:05:09 -0300

    :recycle: Refactor dynamic title script with addEventListener and improved variable naming

--- dynamic-title-blustore.js
+++ dynamic-title-blustore.js
@@ -1,28 +1,28 @@
-window.onload = function () {
+window.addEventListener('load', function() {
   var checkExist = setInterval(function () {
     var shadowHost = document.getElementById('voiceflow-chat');
     if (shadowHost && shadowHost.shadowRoot) {
       clearInterval(checkExist);
 
-      var chatObserver = new MutationObserver(function (mutations) {
+      var titleObserver = new MutationObserver(function (mutations) {
         mutations.forEach(function (mutation) {
           mutation.addedNodes.forEach(function (node) {
             var assistantTitle = node.querySelector('.vfrc-assistant-info--title');
             if (assistantTitle) {
               assistantTitle.innerText = 'MARÍA';
-              chatObserver.disconnect();
+              titleObserver.disconnect();
             }
           });
         });
       });
 
-      chatObserver.observe(shadowHost.shadowRoot, { childList: true, subtree: true });
+      titleObserver.observe(shadowHost.shadowRoot, { childList: true, subtree: true });
 
       var existingTitle = shadowHost.shadowRoot.querySelector('.vfrc-assistant-info--title');
       if (existingTitle) {
         existingTitle.innerText = 'MARÍA';
-        chatObserver.disconnect();
+        titleObserver.disconnect();
       }
     }
   }, 200);
-}; 
+}); 
